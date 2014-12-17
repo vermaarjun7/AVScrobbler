0d4bca31f74548e14c0d9d272eed842fe332da89
vermaarjun7 <arjunkaran169@gmail.com>
2014-12-17 16:07:55 +0530

    added one more method

--- Tests/tests.py
+++ Tests/tests.py
@@ -5,6 +5,16 @@ import pylast
 
 from mopidy import models
 
+def init(self):
+        self.config = {
+            'AVScrobbler': {
+                'username': 'domingo__chavez',
+                'password': 'secret',
+            }
+        }
+        self.frontend = frontend_lib.ScrobblerFrontend(
+            self.config, mock.sentinel.core)
+
 
 def test_on_start_creates_lastfm_network(self, pylast_mock):
         pylast_mock.md5.return_value = mock.sentinel.password_hash
@@ -16,3 +26,16 @@ def test_on_start_creates_lastfm_network(self, pylast_mock):
             api_secret=frontend_lib.API_SECRET,
             username='alice',
             password_hash=mock.sentinel.password_hash)
+            
+            
+
+def test_does_not_scrobble_tracks_shorter_than_30_sec(self, pylast_mock):
+        self.frontend.lastfm = mock.Mock(spec=pylast.LastFMNetwork)
+        track = models.Track(length=20432)
+        
+        tl_track = models.TlTrack(track=track, tlid=17)
+        self.frontend.track_playback_ended(tl_track, 20432)
+        self.assertEqual(self.frontend.lastfm.scrobble.call_count, 0)
+        
+        
+        
