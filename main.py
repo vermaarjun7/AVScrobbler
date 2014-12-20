a37af3f3ff9a22e340498000039b69273ecb0eb2
vermaarjun7 <arjunkaran169@gmail.com>
2014-12-20 16:50:07 +0530

    main function

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import scrobbler
 
 if __name__ == "__main__":
     api_root = 'https://ws.audioscrobbler.com/2.0/'  # https for mobile
-
-    
-    api_key = '7d57a78411fdf10e0409d678d2a950a2'
+    #apikey of your last fm account
+    #secret key of your last account
+    api_key = '7d57a78411fdf10e0409d678d2a950a2' 
     secret = '6cfcb085801ad30d7ebc1da06544920e'
-
+    #write username and password of lastfm account
     session_key = login('myuser', 'mypassword')
-    submit('Arjun', 'MusicLover')
+    submit('Arjun', 'Verma')
